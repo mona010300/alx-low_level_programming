16913a3721f4cf22b666b616345f3cfc2265ab72
mona010300 <maporisamonalisa@gmail.com>
2021-06-21 03:40:46 -0700

    commit me

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,22 @@
-#!/bin/bash
 #include <unistd.h>
 
-   /**
-    * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
-    * followed by a new line, to standard error.
-    *
-    *Return: Always 1.
-    */
+/**
+ * main - Prints to string
+ *
+ * Return: Always 1
+ */
 int main(void)
 {
-  write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n'
-         59);
-return(1);
-   }
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+_asm_("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
+}
