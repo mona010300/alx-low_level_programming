d409b63c1257dbc62289afa0decdfac62598d86c
mona010300 <maporisamonalisa@gmail.com>
2021-06-21 13:53:27 -0700

    keep these

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chl, chu;
+	
+	chl = 'a';
+	chu = 'A';
+	while (chl <= 'z')
+	{
+		putchar(chl);
+		chl++;
+	}
+	while (chu <= 'Z')
+	{
+		putchar(chu);
+		chu++;
+	}
+	printf("\n");
+	return (0);
+}
