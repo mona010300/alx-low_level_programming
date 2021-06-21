5cb488f66000f0fa761d85f327ba4bbfd9889955
mona010300 <maporisamonalisa@gmail.com>
2021-06-21 02:13:00 -0700

    commit again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *main - print the string in the put function
+ * main - Entry point
  *
- *Description: using the main function
- *this program prints "Programming is like building a multilingual puzzle"
- *Return: 0
+ * Return: Always 0 (Success)
  */
- int main(void)
- {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
- }
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
