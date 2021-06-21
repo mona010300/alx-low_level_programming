8e37c23943a301b7a5078e2870a64bf4064024e1
mona010300 <maporisamonalisa@gmail.com>
2021-06-21 05:34:49 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n 1>&2", 59);
+	write(1, 
+			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+			59);
+
 	return (1);
 }
